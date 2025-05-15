7723696908da0e9591dfca17c2f3181a80677404
Matt Bennett <matt.bennett715@gmail.com>
2025-05-15 15:43:47 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for my NixOS System";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...}:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      linguini = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      matt = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs; 
+        modules = [ ./home.nix ];
+      }; 
+    };
+  };
+}
