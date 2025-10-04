b4a46c899ec9f1757a99ddadabdce3dcbcdd244c
Matt Bennett <matt.bennett715@gmail.com>
2025-10-04 11:47:22 -0600

    Terminal edits. Mainly fuzzy finding and color changes.

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source``
+macchina
+
+set fish_greeting
+
+starship init fish | source
+zoxide init fish | source
